349f066508c30eb84f6b15b9f812de601e512eb9
saumyasaxenaa <saumyasaxena004@gmail.com>
2024-02-19 10:53:39 -0800

    Added frontend feature for adding todo

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,10 +1,44 @@
-import { createContext, useEffect, useState } from "react";
-import { Stack } from "@chakra-ui/react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { Input, InputGroup, Stack } from "@chakra-ui/react";
 
 const TodosContext = createContext({
   todos: [],
   fetchTodos: () => {},
 });
+
+const AddTodo = () => {
+  const [item, setItem] = useState("");
+  const { todos, fetchTodos } = useContext(TodosContext);
+
+  const handleInput = (event) => {
+    setItem(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    const newTodo = {
+      id: todos.length + 1,
+      item: item,
+    };
+    fetch("http://localhost:8000/todos", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newTodo),
+    }).then(fetchTodos);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <InputGroup size="md">
+        <Input
+          pr="4.5rem"
+          type="text"
+          placeholder="Add a todo item"
+          aria-label="Add a todo item"
+          onChange={handleInput}
+        />
+      </InputGroup>
+    </form>
+  );
+};
 
 const Todos = () => {
   const [todos, setTodos] = useState([]);
@@ -18,6 +52,7 @@ const Todos = () => {
   }, []);
   return (
     <TodosContext.Provider value={{ todos, fetchTodos }}>
+      <AddTodo />
       <Stack spacing={5} textAlign="center">
         {todos.map((todo) => (
           <b>{todo.item}</b>
